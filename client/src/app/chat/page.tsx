2215a3e3eeceb9b23a74edfc249f0975802a843c
rahmatillokh <webcoder292@gmail.com>
2024-03-31 17:31:46 +0500

    updated

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,7 +1,5 @@
 import Messages from "@/components/Chat-Threads/Message";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import Image from "next/image";
-import logo from "@/assets/Untitled_logo_1_free-file (1).jpg";
 
 const ChatPage = () => {
   return (
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -6,8 +6,10 @@ import { useCookies } from "react-cookie";
 import { shallow } from "zustand/shallow";
 import SearchBar from "./SearchBar";
 import ChatList from "./ChatList";
+import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
+  const router = useRouter();
   const [cookie, setCookie] = useCookies(["user"]);
   const { myUser, setUser } = useUser(
     (state) => ({ myUser: state.myUser, setUser: state.setUser }),
@@ -17,6 +19,10 @@ const Sidebar = () => {
   useEffect(() => {
     fetchUser(cookie, setUser);
   }, []);
+
+  if (!myUser) {
+    router.push("/");
+  }
 
   return (
     <div className="w-full md:!block sidebar z-10 border-r-2 border-slate-400 md:w-1/2 lg:w-1/3 p-3 bg-white dark:bg-gray-800 h-screen overflow-auto no-scrollbar">
